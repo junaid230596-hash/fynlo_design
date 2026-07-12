03505ac8b8d143aeae9f719336a8bfc75e88b2aa
Junaid Sheikh <junaid230596@gmail.com>
2026-07-12 18:42:31 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -14,7 +14,7 @@
 
 const FYNLO_CONFIG = {
   //PRODUCTION_API_URL: 'https://fynlo-production-3664.up.railway.app',
-  PRODUCTION_API_URL: 'https://fynlo-design.vercel.app/',
+  PRODUCTION_API_URL: 'https://fynlo-new.onrender.com',
 
   // Used automatically when running on localhost — no need to change this.
   LOCAL_API_URL: 'http://localhost:3000',
